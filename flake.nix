636f39135c69f79464c677eb05d7df95f87cfef9
erikbackman <erikbackman@users.noreply.github.com>
2021-05-01 21:26:46 +0200

    cleanup

--- flake.nix
+++ flake.nix
@@ -1,10 +1,12 @@
 {
   description = "hello";
+
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     devshell.url = "github:numtide/devshell/master";
   };
+
   outputs = { self, nixpkgs, flake-utils, devshell  }:
     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let
@@ -12,12 +14,13 @@
           inherit system;
           overlays = [ devshell.overlay ];
         };
-        haskellPackages = pkgs.haskellPackages;
+
         tooling = with pkgs; [
           haskellPackages.haskell-language-server
           ghc
           cabal-install
         ];
+
       in {
         devShell = pkgs.devshell.mkShell {
           name = "hello-nix";
@@ -28,5 +31,6 @@
           { inherit pkgs;
             compiler = "ghc884";
           };
-      }); # // { overlay = import ./overlay.nix {}; };
+      }
+    ); # // { overlay = import ./overlay.nix {}; };
 }
